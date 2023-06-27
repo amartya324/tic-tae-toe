4998d5b9d9c8f8ad70bde3c837af1c5d06e251a0
Amartya Kumar <amartya324@gmail.com>
2023-06-27 14:45:28 +0530

    Changes in Background

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -9,7 +9,7 @@ const History = ({ history, moveTo, currentMove }) => {
               className={`btn-move ${currentMove === index ? 'active' : ''}`}
               onClick={() => moveTo(index)}
             >
-              {index === 0 ? 'Start new Game' : `Go to move #${index}`}
+              {index === 0 ? 'Go to game start' : `Go to move #${index}`}
             </button>
           </li>
         ))}
